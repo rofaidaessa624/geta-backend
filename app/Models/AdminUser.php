5f95a967eb0999492d868cf8f97443baa2a30131
rofaidaessa <rofaidaessa6@gmail.com>
2026-01-09 21:59:19 +0200

    test

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -2,25 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable; // مهم
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class AdminUser extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    protected $table = 'admin_users';
+    protected $table = "admin_users";
 
     protected $fillable = [
         'name',
         'email',
         'password',
-        'api_token',
     ];
 
     protected $hidden = [
         'password',
-        'api_token',
+        'remember_token',
     ];
 }
